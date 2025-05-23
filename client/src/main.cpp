2e6db60ae1d8cc653c0b15b4595dd4a620be4235
João Freitas <joaj.freitas@gmail.com>
2025-05-23 17:29:20 +0100

    Add tcp client socket

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,6 +1,33 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-    std::cout << "print" << std::endl;    
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(7777);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr))) {
+        perror("connect");
+        return 1;
+    }
+
+    const char* message = "CAP LS 302";
+    send(sock, message, strlen(message), 0);
+
+    close(sock);
+    std::cout << "print" << std::endl;
 }
